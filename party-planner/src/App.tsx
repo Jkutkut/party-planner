a87e742921c7174e3f69ee6a5911ec559d3db01d
Jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-07-29 00:22:06 +0200

    bugfix: user selection working. Let's add persistance

--- party-planner/src/App.tsx
+++ party-planner/src/App.tsx
@@ -2,66 +2,40 @@ import { useState } from 'react';
 import Calendar from './components/calendar/Calendar'
 import SessionHandler from './model/session/SessionHandler';
 import SessionCreation from './components/session/SessionCreation';
-import UserModel from './model/user/UserModel';
+import UserSelector from './components/user/UserSelector';
 
 function App() {
   const [sessionHandler] = useState<SessionHandler>(SessionHandler.getInstance());
-  const [user, setUser] = useState<number>(0);
   const [session, setSession] = useState(sessionHandler.getCurrentSession());
+
+  const [userIdx, setUserIdx] = useState<number>(-1);
 
   if (!session) {
     return <SessionCreation
       setSession={(session) => {
         sessionHandler.setCurrentSession(session.getName());
         setSession(session);
+        // TODO set user
       }}
     />;
   }
-  const users = session.getUsers();
 
   const selectSession = () => {
     sessionHandler.stopCurrentSession();
     setSession(null);
   };
 
-  const addUser = () => {
-    const userInput = document.getElementById('userInput') as HTMLInputElement;
-    const username = userInput.value.trim();
-    if (username.length === 0) {
-      alert('User name cannot be empty!');
-      return; // TODO handle this better
-    }
-    if (users.find(user => user.getName() === username)) {
-      alert('User already exists!');
-      return; // TODO handle this better
-    }
-    session.getUsers().push(new UserModel(username));
-    userInput.value = '';
-    userInput.focus();
-    setUser(users.length);
-  }
-
   return (
     <>
       <button onClick={selectSession}>Session selector</button>
       <div style={{float: 'right'}}>
-        <h5>DEBUG: {user}</h5>
-        {/* TODO fix select bug */}
-        <select id="userSelector" onChange={(e) => setUser(parseInt(e.target.value))} value={user}>
-          <option value={-1}>Select user</option>
-          {users.map((user, index) => {
-            return <option key={index} value={index}>{user.getName()}</option>
-          })}
-        </select>
-        <input id='userInput' type="text" placeholder="Add user" 
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              addUser();
-            }
-          }}
+        <UserSelector
+          session={session}
+          userIdx={userIdx}
+          setUserIdx={setUserIdx}
         />
-        <button onClick={addUser}>Add</button>
       </div>
+      <p>User: {userIdx} -- {userIdx > 0 && session.getUsers()[userIdx].getName() || "None"}</p>
       <Calendar calendar={session.getCalendar()}/>
     </>
   )
