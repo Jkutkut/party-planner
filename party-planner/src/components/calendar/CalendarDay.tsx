1b2d8bdc1f41ee2af41bd70e771edfd294af7cdf
Jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-08-01 23:54:46 +0200

    feat: Calendar bootstrapped

--- party-planner/src/components/calendar/CalendarDay.tsx
+++ party-planner/src/components/calendar/CalendarDay.tsx
@@ -3,40 +3,53 @@ import CalendarDayModel from "../../model/calendar/CalendarDayModel";
 import Session from "../../model/session/Session";
 
 interface Props {
-    day: CalendarDayModel;
-    session: Session;
+  day: CalendarDayModel;
+  session: Session;
 }
 
 const CalendarDay = ({ day, session}: Props) => {
-    const users = session.getUsers();
-    const [dayUsers, setDayUsers] = useState<number[]>(day.getUsers());
+  const users = session.getUsers();
+  const [dayUsers, setDayUsers] = useState<number[]>(day.getUsers());
 
-    const handleClick = () => {
-        const currentUser = session.getCurrentUserIdx();
-        if (currentUser < 0) {
-            return;
-        }
-        console.debug('Clicked on day ' + day.toString());
-        day.toggleUser(currentUser);
-        setDayUsers([...day.getUsers()]);
-    };
+  const handleClick = () => {
+    const currentUser = session.getCurrentUserIdx();
+    if (currentUser < 0) {
+      return;
+    }
+    console.debug('Clicked on day ' + day.toString());
+    day.toggleUser(currentUser);
+    setDayUsers([...day.getUsers()]);
+  };
 
-    return <div onClick={handleClick}>
-        <hr />
-        <h2>{day.toString()}</h2>
-        {dayUsers.length === 0 &&
-            <p>No users</p> ||
-            <ul>
-                {dayUsers.map((userIdx, index) => {
-                    if (userIdx >= 0) {
-                        return <li key={index}>{users[userIdx].getName()}</li>;
-                    }
-                    return null;
-                })}
-            </ul>
+  let nbrUsers: string;
+  if (dayUsers.length === 0)
+    nbrUsers = 'No users';
+  else if (dayUsers.length === 1)
+    nbrUsers = '1 user';
+  else
+    nbrUsers = dayUsers.length + ' users';
+
+  return (
+    <div className="card" style={{marginTop: "0.5rem"}} onClick={handleClick}>
+      <div className="card-body">
+        <h5 className="card-title">
+          {day.toString()}
+        </h5>
+        <h6 className="card-subtitle mb-2 text-muted">
+          {nbrUsers}
+        </h6>
+        {dayUsers.length > 0 &&
+          <div className="row">
+            {dayUsers.map((userIdx, index) => 
+              <div key={index} className="col-4">
+                {users[userIdx].getName()}
+              </div>
+            )}
+          </div>
         }
-        <hr />
-    </div>;
+      </div>
+    </div>
+  );
 };
 
 export default CalendarDay;
